e90025cdd5fab9daa190f55e2ec1ed0c66f302c3
Pentesting-28 <mpenav28@gmail.com>
2026-03-10 15:21:49 -0400

    feat: Collect and include thermal sensor data in system snapshots.

--- src/collector/system.rs
+++ src/collector/system.rs
@@ -1,8 +1,8 @@
-use sysinfo::{System, Networks};
+use sysinfo::{System, Networks, Components};
 use crate::models::metrics::{
     SystemSnapshot,
     NetworkMetrics,
-    // ThermalMetrics
+    ThermalMetrics
 };
 use chrono::Utc;
 
@@ -14,7 +14,7 @@ pub fn collect_system_data() -> SystemSnapshot {
     let uptime = System::uptime();
     let networks = Networks::new_with_refreshed_list();
     let mut network_metrics: Vec<NetworkMetrics> = Vec::new();
-    
+
     for (interface_name, data) in networks.iter() {
         network_metrics.push(NetworkMetrics {
             interface: interface_name.clone(),
@@ -26,11 +26,21 @@ pub fn collect_system_data() -> SystemSnapshot {
         });
     }
 
+    let components = Components::new_with_refreshed_list();
+    let mut thermal_metrics: Vec<ThermalMetrics> = Vec::new();
+
+    for component in &components {
+        thermal_metrics.push(ThermalMetrics {
+            label: component.label().to_string(),
+            temperature: component.temperature().unwrap_or(0.0),
+        });
+    }
+
     SystemSnapshot{
         hostname,
         uptime,
         network: network_metrics,
-        temperatures: Vec::new(),
+        temperatures: thermal_metrics,
         timestamp: Utc::now().timestamp(),
     }
 }
