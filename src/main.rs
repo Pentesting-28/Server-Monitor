c2492a8d66fef08f135b886bf5bbceb5c67012f7
Pentesting-28 <mpenav28@gmail.com>
2026-03-11 18:07:37 -0400

    feat: Initialize SQLite database and add its file to .gitignore.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,22 @@ mod models;
 
 // Import functions
 use collector::system::collect_system_data;
+use db::database::init_db;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
     tracing_subscriber::fmt::init();
-    
     tracing::info!("Starting server monitor");
+    tracing::info!("Initializing database...");
+
+    let db_url = "sqlite:monitor.db?mode=rwc";
+    let _pool = init_db(db_url).await?;
+    
+    tracing::info!("Database initialized successfully");
+
     let snapshot = collect_system_data();
     tracing::info!("Snapshot Collector: {:?}", snapshot);
+
+    Ok(())
 }
